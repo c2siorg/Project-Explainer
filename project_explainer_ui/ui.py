eaad6bc11f609dc6dfdec988730d6d1514885fbd
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2024-02-29 20:56:41 +0200

    Update UI to show more datils about error

--- project_explainer_ui/ui.py
+++ project_explainer_ui/ui.py
@@ -2,10 +2,13 @@ import gradio as gr
 from gh_explainer import Explainer
 
 def summarize(summarization_type, github_project_url, github_project_branch="main", huggingface_model_id="gpt2"):
-    gptExplainer = Explainer(huggingface_model_id)
-    if summarization_type == "brief":
-        return gptExplainer.brief(github_url=github_project_url, branch=github_project_branch)["summary"]
-    return gptExplainer.outline(github_url=github_project_url, branch=github_project_branch)["summary"]
+    try:
+        gptExplainer = Explainer(huggingface_model_id)
+        if summarization_type == "brief":
+            return gptExplainer.brief(github_url=github_project_url, branch=github_project_branch)["summary"]
+        return gptExplainer.outline(github_url=github_project_url, branch=github_project_branch)["summary"]
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
 demo = gr.Interface(
     fn=summarize,
