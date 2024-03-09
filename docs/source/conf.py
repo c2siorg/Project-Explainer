061ca1aed78b2b7cad5b6b4ba87047a76ce0675d
Yeshvanth Raju Kurapati <ethanhunt@Yeshvanths-MacBook-Air.local>
2024-03-08 18:16:34 -0800

    Docs updated

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('/Users/ethanhunt/Project-Explainer'))
+
+project = 'PROJECT-EXPLAINER'
+copyright = '2024, C2SI'
+author = 'C2SI'
+release = '27 Aug, 2023'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'Python, Jupyter Notebook'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
