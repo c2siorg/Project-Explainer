e0e5331a64337722b5a32ddc7f13c28625e4b16e
debrupf2946 <f20212946@goa.bits-pilani.ac.in>
2024-07-05 00:04:38 +0530

    added exception handling to the codes

--- graph_rag/graph_builder/knowledgeGraph.py
+++ graph_rag/graph_builder/knowledgeGraph.py
@@ -26,37 +26,44 @@ def build_graph(
         embeddings: Hugging-Face Embeddings model name default: microsoft/codebert-base
 
     Returns:
-        Knowledge Graph-index,also saves html visualization file
-
+        Knowledge Graph-index, also saves html visualization file
     """
-    graph_store = SimpleGraphStore()
-    storage_context = StorageContext.from_defaults(graph_store=graph_store)
-    index = KnowledgeGraphIndex.from_documents(
-        documents,
-        max_triplets_per_chunk=max_triplets_per_chunk,
-        llm=llm,
-        embed_model=HuggingFaceEmbedding(model_name=embeddings),
-        storage_context=storage_context,
-    )
-    print("KG built succesfully!")
-    os.makedirs("results", exist_ok=True)
-    g = index.get_networkx_graph()
-    net = Network(notebook=True, cdn_resources="in_line", directed=True)
-    net.from_nx(g)
-    net.show("Graph_visualization.html")
-    return index
+    try:
+        graph_store = SimpleGraphStore()
+        storage_context = StorageContext.from_defaults(graph_store=graph_store)
+        index = KnowledgeGraphIndex.from_documents(
+            documents,
+            max_triplets_per_chunk=max_triplets_per_chunk,
+            llm=llm,
+            embed_model=HuggingFaceEmbedding(model_name=embeddings),
+            storage_context=storage_context,
+        )
+        print("KG built successfully!")
+
+        os.makedirs("results", exist_ok=True)
+        g = index.get_networkx_graph()
+        net = Network(notebook=True, cdn_resources="in_line", directed=True)
+        net.from_nx(g)
+        net.show("Graph_visualization.html")
+        return index
+    except Exception as e:
+        print(f"Error building graph: {e}")
+        return None
 
 
 def save_index(index, output_dir_path: str = "Results/"):
     """
-    serializes the index object,so that it can be loaded and used later
+    Serializes the index object, so that it can be loaded and used later
     Args:
-        index: Grpah-Index object
+        index: Graph-Index object
 
     Returns:
-        saves pickle file of the Grpah-Index
+        Saves pickle file of the Graph-Index
     """
-    os.makedirs(output_dir_path[:-1], exist_ok=True)
-    with open(output_dir_path + "graphIndex", "wb") as f:
-        pickle.dump(index, f)
-    print("Index saved succesfully!")
+    try:
+        os.makedirs(output_dir_path[:-1], exist_ok=True)
+        with open(output_dir_path + "graphIndex", "wb") as f:
+            pickle.dump(index, f)
+        print("Index saved successfully!")
+    except Exception as e:
+        print(f"Error saving index: {e}")
